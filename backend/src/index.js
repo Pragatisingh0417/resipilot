733a648a74b9ecbb3606bef169313564d9e6cabb
Pragati Singh <pragatisingh0417@gmail.com>
2026-04-30 13:03:58 +0530

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 app.use(cors({
   origin: [
-    "http://localhost:3000",
+    
     "https://resipilot.vercel.app/login"
   ],
   credentials: true
